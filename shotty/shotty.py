1ff4b54e666e37a6b55d0acf2bd69a6340265d07
Kalpit Parikh <parikh.kalpit@gmail.com>
2018-10-10 06:42:50 -0700

    update the script to use click modulex, and show more information about the instance

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,23 @@
 import boto3
-if __name__ == '__main__':
+import click
 
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
 
+
+@click.command()
+def list_instances():
+    "List EC2 Instances"
     for i in ec2.instances.all():
-        print (i)
+        print (','.join((
+        i.id,
+        i.instance_type,
+        i.placement['AvailabilityZone'],
+        i.state['Name'],
+        i.public_dns_name
+        )))
+
+    return
+
+if __name__ == '__main__':
+     list_instances ()
